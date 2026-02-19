ac7fc945ea2f241165be8419a972fb07f06a74a3
AiranCana <airancc2005@gmail.com>
2026-02-19 12:12:51 +0100

    all

---
+++ README.md
@@ -0,0 +1,1 @@
+Airan: movements
---
+++ movements/movements.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movements.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acanadil <acanadil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 13:20:40 by acanadil          #+#    #+#             */
+/*   Updated: 2026/02/19 11:39:25 by acanadil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef MOVEMENTS_H
+# define MOVEMENTS_H
+
+# include "../libft/libft.h"
+
+t_list	*swap(t_list *swap);
+void	ss(t_list **b, t_list **a);
+
+#endif
