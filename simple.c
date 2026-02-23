9f1c2e7b2f323a864b723c142cc942ec8e5685eb
raqroca- <raqroca-@student.42madrid.com>
2026-02-23 17:29:04 +0100

    simple

--- simple.c
+++ simple.c
@@ -6,91 +6,53 @@
 /*   By: raqroca- <raqroca-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 10:41:14 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/02/20 12:20:20 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/02/23 17:24:56 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_three(t_stack **stack)
-{
-	int	first;
-	int	second;
-	int	third;
-
-	first = (*stack)->stacka->num;
-	second = (*stack)->stacka->next->num;
-	third = (*stack)->stacka->next->next->num;
-	if (first < second && second < third)//sorted
-		return;
-	if (first > second && first > third)//first bigger
-	{
-		ra(*stack);// rorate
-		if ((*stack)->stacka->num > (*stack)->stacka->next->num)
-			sa(*stack);
-	}
-	else if (second > first && second > third)//second bigger
-	{
-		rra(*stack);//reverse rotte
-		if ((*stack)->stacka->num > (*stack)->stacka->next->num)
-			sa(*stack);
-	}
-	else//third bigger
-	{
-		sa(*stack);
-	}
-}
-
-/**
-order 5 numbers:
-
-find the 2 smaller nombers and move to stack b
-sort_three
-insert de 2 smaller numbers in order
- */
-
-void	sort_five(t_stack **stack)
+void	simple(t_stack **stack)
 {
 	int	min;
-	int	min_pos;
-	int	second_min;
-	int	i;
-
-	min = get_min((*stack)->stacka);
-	min_pos = 0;
-	i = 0;
-	while ((*stack)->stacka->num != min)
-	{
-		ra(*stack);
-		min_pos++;
-	}
-	pb(*stack); //push min to B
-	second_min = get_min((*stack)->stacka);
-	i = 0;
-	while ((*stack)->stacka->num != second_min)
-	{
-		ra(*stack);
-		i++;
-	}
-	pb(*stack);
-	sort_three(stack);//sort 3 number left
-	pa(*stack); // instr el second-min
-	pa(*stack); // inset el min
-	if ((*stack)->stacka->num > (*stack)->stacka->next->num)
-		sa(*stack);//adjust is is necesary
-}
-
-void	simple(t_stack **order)
-{
+	int	pos;
 	int	size;
 
-	if (!order || !(*order) || !(*order)->stacka)
-		return;
-	size = ft_lstsize((*order)->stacka);
-	if (is_sorted((*order)->stacka))
-		return;
-	if (size <= 3)
-		sort_three(order);
-	else if (size <= 5)
-		sort_five(order);
+	if (!stack || !(*stack) || !(*stack)->stacka)
+		return ;
+	while ((*stack)->stacka && !is_sorted((*stack)->stacka))//no emty no order
+	{
+		size = ft_lstsize((*stack)->stacka);
+		min = get_min((*stack)->stacka);
+		pos = get_position((*stack)->stacka, min);
+		if ((*stack)->stackb != NULL)// b not empty
+		{
+			if (pos <= size / 2)//first half
+			{
+				while ((*stack)->stacka->num != min)
+					rr(*stack);
+			}
+			else//second half
+			{
+				while ((*stack)->stacka->num != min)
+					rrr(*stack);
+			}
+		}
+		else// empty b
+		{
+			if (pos <= size / 2)//first half- a up
+			{
+				while ((*stack)->stacka->num != min)
+					ra(*stack);
+			}
+			else
+			{//second half
+				while ((*stack)->stacka->num != min)
+					rra(*stack);
+			}
+		}
+		pb(*stack);// pass min b
+	}
+	while ((*stack)->stackb != NULL)//return a and b, in order asc
+		pa(*stack);
 }
