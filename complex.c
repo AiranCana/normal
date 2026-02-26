430eaa19b5985c318ec662f5c7ff228eab30abed
raqroca- <raqroca-@student.42madrid.com>
2026-02-26 17:34:45 +0100

    assign_pos and count_bits

---
+++ complex.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   complex.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 12:26:07 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/02/26 16:18:01 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+/*
+** This function calculates the number of bits needed to represent 
+** the largest integer in the stack (max_n). 
+**
+** The right shift operator (>>) is used to analyze 
+** the binary structure of the number:
+** 1. In each iteration, we evaluate the value resulting from shifting 'n_bits' 
+** positions to the right.
+** 2. The shift removes the least significant bit (the one on the right).
+** 3. The loop ends when the resulting value is equal to 0, indicating 
+** that all significant bits have been processed.
+**
+** Technical analysis (Example with max_n = 5):
+** - [1] [0] [1] (Value: 5) -> Shift  0: 5 != 0 (True) -> n_bits = 1
+** -  0  [1] [0] (Value: 2) -> Shift  1: 2 != 0 (True) -> n_bits = 2
+** -  0   0  [1] (Value: 1) -> Shift  2: 1 != 0 (True) -> n_bits = 3
+** -  0   0   0  (Value: 0) -> Offset 3: 0 != 0 (False) -> Returns 3
+
+
+*/
+
+static int	count_bits(int max_n)
+{
+	int	n_bits;
+
+	n_bits = 0;
+	while ((max_n >> n_bits) != 0)
+	{
+		n_bits++;
+	}
+	return (n_bits);
+}
+
+void	assign_pos(t_list *stacka)
+{
+	t_list	*current;
+	t_list	*compare;
+	int		count;
+
+	current = stacka;
+	while (current != NULL)
+	{
+		count = 0;
+		compare = stacka;
+		while (compare != NULL)
+		{
+			if (compare->num < current->num)
+				count++;
+			compare = compare->next;
+		}
+		current->pos = count;
+		current = current->next;
+	}
+}
