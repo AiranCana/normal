5ba8b3a96547382876221f7d732aac3629404e15
raqroca- <raqroca-@student.42madrid.com>
2026-03-02 15:49:00 +0100

    complex with comment

--- complex.c
+++ complex.c
@@ -6,7 +6,7 @@
 /*   By: raqroca- <raqroca-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 12:26:07 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/03/02 13:35:17 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/03/02 15:47:26 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,14 @@
 ** the Radix algorithm to sort based on relative order rather than 
 ** the raw 'num' values, which could be negative or very large.
 
-
+** complex
+** This function sorts large lists (such as 100 or 500 numbers):
+** 1. First, it assigns each number a position (0, 1, 2...) according to its size.
+** 2. Then, it checks those numbers ‘bit by bit’ (like looking at each digit individually).
+** 3. If a number has a “0” in that bit, it moves it to Stack B.
+** 4. If it has a “1”, it leaves it in Stack A, sending it to the end.
+** 5. At the end of each round, it returns everything from B to Stack A.
+** 6. It repeats this until all the numbers are in order.
 */
 
 static int	count_bits(int max_n)
@@ -81,6 +88,8 @@ void	complex(t_stack **stack)
 
 	if (!stack || !(*stack) || !(*stack)->stacka)
 		return ;
+	if (is_sorted((*stack)->stacka))
+		return ;
 	assign_pos((*stack)->stacka);
 	size = ft_lstsize((*stack)->stacka);
 	max_bits = count_bits(size - 1);
@@ -90,10 +99,10 @@ void	complex(t_stack **stack)
 		j = 0;
 		while (j < size)
 		{
-			if ((((*stack)->stacka->pos >> i) & 1) == 1)
-				ra(stack);
-			else
+			if ((((*stack)->stacka->pos >> i) & 1) == 0)
 				pb(stack);
+			else
+				ra(stack);
 			j++;
 		}
 		while ((*stack)->stackb != NULL)
